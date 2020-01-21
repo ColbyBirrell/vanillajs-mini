26b365224c99506c7885cae8a1e6bcc0ee039f92
ColbyBirrell <colby.birrell@gmail.com>
2020-01-21 13:55:30 -0700

    saving for ref

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log("connected");
+
+function addTodo(event) {
+  event.preventDefault();
+  const item = document.createElement("li");
+  item.innerText = document.getElementById("item").value;
+  item.addEventListener("click", completeTodo);
+
+  const button = document.createElement("button");
+  button.innerText = "x";
+  button.addEventListener("click", removeTodo);
+  //   button.style.background = "cyan"; <<< javascript styling overrides css
+  item.append(button);
+
+  const list = document.querySelector("ul");
+  list.appendChild(item);
+}
+
+function removeTodo(event) {
+  event.target.parentNode.remove();
+}
+
+function completeTodo(event) {
+  const value = event.target.getAttribute("aria-checked");
+
+  if (value !== "true") {
+    event.target.setAttribute("aria-checked", "true");
+  } else {
+    event.target.setAttribute("aria-checked", "false");
+  }
+}
+
+document.querySelector("form").addEventListener("submit", addTodo);
